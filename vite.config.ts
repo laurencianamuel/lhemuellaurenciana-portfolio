43407a9135a49aadd62fc8e05b11cb8456956fff
laurencianamuel <laurencianamuel@gmail.com>
2026-09-20 15:12:20 +0800

    update resume

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: '/lhemuellaurenciana-portfolio/',
+  base: '/portfolio/',
   plugins: [react()].filter(Boolean),
   resolve: {
     alias: {
